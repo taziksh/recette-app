0dd117447109ad47176bd49062fc37ea007d30e6
Tazik <tshahjah@uwaterloo.ca>
2021-08-21 15:59:10 -0400

    Feat: tab navigation

--- App.js
+++ App.js
@@ -1,30 +1,56 @@
 import React from 'react';
-import { NativeBaseProvider, VStack, HStack, Center, Heading, Text, Box, Divider, Icon, Image } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { ListItem, Avatar } from 'react-native-elements';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NativeBaseProvider, VStack, HStack, Center, Heading, Text, Box, Divider, Icon, Image, Button } from 'native-base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <NativeBaseProvider>
-      <VStack space={4} alignItems="center">
-      <Heading alignSelf="center">🧑‍🍳 Recette</Heading>
-      <Text alignSelf="center" fontSize="md" >        
-    <Text style={{ textDecorationColor: "blue", textDecorationLine: "underline", textDecorationStyle: "wavy" }} fontSize="lg" >Recipes</Text>
-      </Text>
-      
-      <Box bg="primary.400" p="12" rounded="md">
-        What I got today 
-      </Box>
-      <Box bg="secondary.400" p="12" rounded="lg" >
-        What I ate today
-      </Box>
-      <Box bg="emerald.400" p="12" rounded="lg" >
-        What a great day
-      </Box>
-      
-            
-      </VStack>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-      
-    </NativeBaseProvider>
+          if (route.name == 'Home') {
+            iconName = focused
+              ? 'fridge' : 'fridge-outline';
+          } else if (route.name == 'Pictures') {
+            iconName = focused 
+              ? 'camera' : 'camera-outline';
+          }
+
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Pictures" component={PicturesScreen} />
+      </Tab.Navigator>
+    </NativeBaseProvider>  
+    </NavigationContainer>
   );
 }
 
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <VStack> 
+      <Text> Home! </Text>
+      </ VStack>
+  );
+}
+
+const PicturesScreen = () => {
+  return (
+  <VStack>
+  <Text>
+    This is a screen for managing your pictures.
+  </Text>
+  </VStack>
+  )
+}
