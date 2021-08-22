8983e65c647f7c0e5eaedf65bd2b38887f86a6ae
Tazik <tshahjah@uwaterloo.ca>
2021-08-21 22:08:42 -0400

    GroceryList and CameraButton skeletons are up

--- App.js
+++ App.js
@@ -3,54 +3,114 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ListItem, Avatar } from 'react-native-elements';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { NativeBaseProvider, VStack, HStack, Center, Heading, Text, Box, Divider, Icon, Image, Button } from 'native-base';
+import { NativeBaseProvider, VStack, HStack, Center, Heading, Text, Button, Box, Divider, Icon, Image  } from 'native-base';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
-    <NavigationContainer>
     <NativeBaseProvider>
+    <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({route}) => ({
+        screenOptions={
+          ({route}) => ({
+          headerShown: false,    
+          cardStyle: { backgroundColor: '#fff' },
           tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
           if (route.name == 'Home') {
             iconName = focused
-              ? 'fridge' : 'fridge-outline';
-          } else if (route.name == 'Pictures') {
+              ? 'home' : 'home-outline';
+          } else if (route.name == 'Receipts') {
             iconName = focused 
-              ? 'camera' : 'camera-outline';
+              ? 'receipt' : 'receipt-outline';
+          } else if (route.name == 'Recipes') {
+            iconName = focused 
+              ? 'fast-food' : 'fast-food-outline';
+
           }
 
-          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          return <Ionicons name={iconName} size={size} color={color} />;
           },
-        })}
+        })
+        }
       >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Pictures" component={PicturesScreen} />
+        <Tab.Screen name="Receipts" component={ReceiptsScreen} />
+        <Tab.Screen name="Recipes" component={RecipesScreen} />
       </Tab.Navigator>
-    </NativeBaseProvider>  
     </NavigationContainer>
+    </NativeBaseProvider>  
   );
 }
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => {
+const list = [  {    name: 'Tea',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',    subtitle: '40oz'  },  {    name: 'Tapioca Pearls',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',    subtitle: '500g'  }, {    name: 'Quinoa',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',    subtitle: '500g'  }, {    name: 'Avocado toast',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',    subtitle: '500g'  }, {    name: 'Soy sauce',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',    subtitle: '40oz'  },{    name: 'Oranges',    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',    subtitle: '40oz'  },]
+
+const GroceryList = () => {
   return (
-    <VStack> 
-      <Text> Home! </Text>
-      </ VStack>
+    <VStack>
+      {
+        list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{uri: l.avatar_url}} />
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle} </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+
+        ))
+      }
+    </VStack>
   );
 }
 
-const PicturesScreen = () => {
+const HomeScreen = () => {
+  return (
+    <VStack bg="white" space={4}> 
+    <Heading alignSelf="center" pt={8}>Home</Heading>
+      <HStack justifyContent="space-around" py={4} px={3}>
+        <Button endIcon={<Icon as={Ionicons} name="arrow-forward" size={4}/>} bg="primary.400"> Receipts </Button>
+        <Button endIcon={<Icon as={Ionicons} name="arrow-forward" size={4}/>} bg="secondary.400"> Recipes </Button>
+      </HStack>
+      <GroceryList/>
+    </ VStack>
+  );
+}
+
+const CaptureCard = () => {
+  return (
+    <VStack> 
+      <Box shadow={2} bg="white" p="12" rounded="sm" >
+      <Button bg="primary.400" alignSelf="center"
+        startIcon={<Icon as={MaterialCommunityIcons} name="camera" size={5} />}
+      >
+        Upload
+      </Button>
+      </Box>
+      
+    </VStack>
+  );
+}
+
+const ReceiptsScreen = () => {
   return (
   <VStack>
-  <Text>
-    This is a screen for managing your pictures.
-  </Text>
+    <Heading alignSelf="center" color="blue.500">
+      Receipts
+    </Heading>
+    <CaptureCard/>
   </VStack>
   )
+}
+
+const RecipesScreen = () => {
+  return (
+  <VStack>
+    <CaptureCard> </CaptureCard>
+  </VStack>
+  )
+
 }
